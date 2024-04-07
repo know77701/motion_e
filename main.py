c349ae63b268caff152e4cced432969e0f6a79c0
know77701 <know77701@gmail.com>
2024-04-07 10:45:36 +0900

    update code

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from pywinauto import application
-from pywinauto import keyboard
 import time
 
 app = application.Application(backend='uia')
-app.start("C:\Motion\Motion_E\Motion_E.exe")
 
 
 def login(title, id):
@@ -11,19 +9,19 @@ def login(title, id):
     login_window.child_window(auto_id=id).click()
 
 
-time.sleep(1)
-if app.connect(path="C:\Motion\Motion_E\Motion_E.exe"):
-    print('connect pass')
-    login_window = app.window(title="로그인")
+try:
+    app.connect(path="C:\Motion\Motion_E\Motion_E.exe")
     motion_window = app.window(title='모션.ver[1.0.0.127]ipc://Motion_E-64bit')
+    print('기존 앱 연결')
 
+except application.ProcessNotFoundError:
+    app.start("C:\Motion\Motion_E\Motion_E.exe")
+    login_window = app.window(title="로그인")
+    time.sleep(1)
     if login_window.exists():
         login('로그인', 'btnLogin')
-        print("login pass")
-else:
-    print("connect fail")
+        print("로그인 성공")
 
 time.sleep(5)
+print('모션 연결 성공')
 print("-------------------")
-
-motion_window['닫기'].click()
