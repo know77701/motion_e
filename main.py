d573322af7e8cd0352e20879e053887dabdb479c
know77701 <know77701@gmail.com>
2024-04-09 16:04:34 +0900

    dashBoard update

--- main.py
+++ main.py
@@ -18,52 +18,75 @@ def appConnect():
         print('기존 앱 연결')
 
     except application.ProcessNotFoundError:
-
         app.start("C:\\Motion\\Motion_E\\Motion_E.exe")
-
         time.sleep(3)
         login('로그인', 'btnLogin')
         print("로그인 성공")
 
 
 appConnect()
-
+time.sleep(5)
 MotionTitle= {}
 windows = Desktop(backend="uia").windows()
+
 for window in windows:
-    if('모션.ver' in window.window_text()):
-        MotionTitle = window.window_text()
-        
+    try:
+        if('모션.ver' in window.window_text()):
+            MotionTitle = window.window_text()
+            print('버전찾기 성공')
+    except Exception as e:
+        print('버전 찾기 실패', e)
+            
 
 motion_window = app.window(title=MotionTitle)
-
 print('모션 연결 성공')
 print("-------------------")
+time.sleep(5)
 
-def dashboardreserve(searchName):
-    motion_window.child_window(
-        auto_id="srch-val",  control_type="Edit").type_keys(searchName)
-    motion_window.child_window(title="검색", control_type="Button").click()
-    motion_window.child_window(
-        title="예약하기", control_type="Button", found_index=1).click()
-    time.sleep(1)
-    motion_window.child_window(title="오늘", control_type="Button").click()
-    # motion_window.child_window(
-    #     title="시간", control_type="ComboBox").click_input()
+class dashBoard():
+    def searchUser(searchName):
+        motion_window.child_window(auto_id="srch-val",  control_type="Edit").type_keys(searchName)
+        motion_window.child_window(title="검색", control_type="Button").click()
+    def reserve():
+        try:
+            dashBoard.searchUser('김지헌')
+            time.sleep(1)
+            motion_window.child_window(
+                title="예약하기", control_type="Button", found_index=1).click()
+            time.sleep(1)
+            motion_window.child_window(title="오늘", control_type="Button").click()
+            motion_window.child_window(
+                title="시간", control_type="ComboBox").click_input()
 
-    # motion_window.child_window(
-    #     title="20", control_type="ListItem").click_input()
+            motion_window.child_window(
+                title="20", control_type="ListItem").click_input()
 
-    # motion_window.child_window(
-    #     control_type="ComboBox", found_index=1).select()
-    # motion_window.child_window(title="20", control_type="ListItem").click()
-
-# dashboardreserve('김지헌')
-# motion_window.child_window(
-#         auto_id="srch-val",  control_type="Edit").type_keys('테스트')
+            motion_window.child_window(
+                control_type="ComboBox", found_index=1).select()
+            motion_window.child_window(title="20", control_type="ListItem").click()
+            print("예약 성공: ", e)
+        except Exception as e:
+            keyboard.send_keys('{F5}')
+            print("예약 실패: ", e)
+    
+    def receipt(searchName):
+        try:
+            dashBoard.searchUser('김지헌')
+            time.sleep(1)
+            motion_window.child_window(title="접수하기", control_type="Button", found_index=0).click()
+            time.sleep(5)
+            motion_window.child_window(
+                auto_id="265281",  control_type="Edit").type_keys('접수메모')
+            motion_window.child_window(
+                auto_id="265210",  control_type="Edit").type_keys('메모하나')
+            time.sleep(1)
+            motion_window.child_window(auto_id="btnAcpt", control_type="Button").click()
+            print("접수 성공")
+        except Exception as e:
+            keyboard.send_keys('{F5}')
+            print("접수 실패", e)
 
 class notice:
-    @staticmethod
     def noticeCreate(value):
         try:
             print('공지사항 생성 성공')
@@ -71,6 +94,7 @@ class notice:
             keyboard.send_keys('{ENTER}')
         except Exception as e:
             print('공지사항 생성 실패: ', e)
+            keyboard.send_keys('{F5}')
 
     def noticeDelete():
         try:
@@ -83,6 +107,10 @@ class notice:
             print('공지사항 삭제 실패: ', e)
 
 
-notice.noticeCreate('테스트')
-time.sleep(1)
-notice.noticeDelete()
+dashBoard.receipt('김지헌')
+
+# dashboardAction.reserve('김지헌')
+# time.sleep(5)
+# notice.noticeCreate('테스트')
+# time.sleep(3)
+# notice.noticeDelete()
--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-
-notice.noticeCreate('테스트')
