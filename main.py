dacaf336d1ce8ac06ff44bd5533e5f897629d233
know77701 <know77701@gmail.com>
2024-04-09 14:15:38 +0900

    공지사항 관련 체크리스트 추가

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pywinauto import application
 from pywinauto import Desktop
+from pywinauto import keyboard
 import time
 
 
@@ -18,7 +19,6 @@ def appConnect():
 
     except application.ProcessNotFoundError:
         app.start("C:\\Motion\\Motion_E\\Motion_E.exe")
-        windows = Desktop(backend="uia").windows()
         time.sleep(3)
         login('로그인', 'btnLogin')
         time.sleep(3)
@@ -27,7 +27,7 @@ def appConnect():
 
 appConnect()
 
-
+MotionTitle= {}
 windows = Desktop(backend="uia").windows()
 for window in windows:
     if('모션.ver' in window.window_text()):
@@ -35,7 +35,6 @@ for window in windows:
         
 
 motion_window = app.window(title=MotionTitle)
-print(motion_window)
 print('모션 연결 성공')
 print("-------------------")
 
@@ -47,20 +46,42 @@ def dashboardreserve(searchName):
         title="예약하기", control_type="Button", found_index=1).click()
     time.sleep(1)
     motion_window.child_window(title="오늘", control_type="Button").click()
-    motion_window.child_window(
-        title="시간", control_type="ComboBox").click_input()
+    # motion_window.child_window(
+    #     title="시간", control_type="ComboBox").click_input()
 
-    motion_window.child_window(
-        title="20", control_type="ListItem").click_input()
+    # motion_window.child_window(
+    #     title="20", control_type="ListItem").click_input()
 
-    motion_window.child_window(
-        control_type="ComboBox", found_index=1).select()
-    motion_window.child_window(title="20", control_type="ListItem").click()
+    # motion_window.child_window(
+    #     control_type="ComboBox", found_index=1).select()
+    # motion_window.child_window(title="20", control_type="ListItem").click()
 
-time.sleep(5)
-dashboardreserve('김지헌')
-motion_window.child_window(
-        AutomationId="srch-val",  control_type="Edit").type_keys('테스트')
-motion_window.child_window(auto_id="notice-content").click()
-motion_window.child_window(
-        auto_id="notice-content",  control_type="Edit").type_keys("공지 테스트")
+# dashboardreserve('김지헌')
+# motion_window.child_window(
+#         auto_id="srch-val",  control_type="Edit").type_keys('테스트')
+
+class notice:
+    @staticmethod
+    def noticeCreate(value):
+        try:
+            print('공지사항 생성 성공')
+            motion_window.child_window(auto_id="notice-content",  control_type="Edit").type_keys(value)
+            keyboard.send_keys('{ENTER}')
+        except Exception as e:
+            print('공지사항 생성 실패:', e)
+
+    def noticeDelete():
+        try:
+            motion_window.child_window(title="닫기", control_type="Button", found_index=0).click()
+            time.sleep(1)
+            
+            print(motion_window.child_window(title="radButton1"))
+            motion_window.child_window(auto_id="radButton1", control_type="Button").click()
+            print('공지사항 삭제 성공')
+        except Exception as e:
+            print('공지사항 삭제 실패')
+
+
+if notice.noticeCreate('테스트'):
+    time.sleep(1)
+    notice.noticeDelete()
