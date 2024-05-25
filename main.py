bf67a8fde749be064336b3c5713f43bc73a4f0a1
know77701 <know77701@gmail.com>
2024-05-26 00:00:31 +0900

    멀티 프로세싱 작업 업데이트

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from pywinauto import application, Desktop, keyboard
 import time
 import ctypes
 import sys
-import random
-
+import multiprocessing
 
 MAX_RETRY = 2
 
@@ -86,82 +85,180 @@ class DashBoard():
         motion_window.child_window(
             title="검색", control_type="Button").click()
 
-    def text_edit_popup(user_name, phone):
-        DashBoard.popup_view(user_name)
-        registration_window = motion_app.window(
-            title=MotionStarter.version_search('고객등록'))
-        edit_window = registration_window.child_window(
-            control_type="Edit", auto_id="txtPat_Nm")
-        edit_window.set_edit_text(user_name)
-        if phone:
-            mobile_edit2 = registration_window.child_window(
-                control_type="Edit", auto_id="txtMobile_No2")
-            mobile_edit3 = registration_window.child_window(
-                control_type="Edit", auto_id="txtMobile_No3")
-            mobile_edit2.set_edit_text(phone[3:7])
-            mobile_edit3.set_edit_text(phone[7:11])
-
-    def save_receipt_popup(serach_name, phone_number):
-        DashBoard.text_edit_popup(serach_name, phone_number)
-        receipt_window = motion_app.window(
-            title=MotionStarter.version_search('접수'))
-
-    def save_reserve_popup(serach_name, phone_number):
-        DashBoard.text_edit_popup(serach_name, phone_number)
-
-    def popup_user_save(serach_name, phone_number):
-        try:
-            DashBoard.text_edit_popup(serach_name, phone_number)
-
-        except Exception as e:
-            print('저장 실패 : ', e)
-            time.sleep(1)
-            keyboard.send_keys('{F5}')
-
-    # def popup_user_save(serach_name):
-    #     try:
-    #         DashBoard.popup_view(serach_name)
-    #         DashBoard.text_edit_popup(serach_name)
-
-    #         new_process(registration_window, '고객등록',
-    #                     'radButton1', 'btnSave')
-
-    #     except Exception as e:
-    #         print('저장 실패 : ', e)
-    #         keyboard.send_keys('{F5}')
-
-    def popup_view(serach_name):
-        DashBoard.search_user(serach_name)
+    def popup_view(search_name):
+        DashBoard.search_user(search_name)
         register_btn = motion_window.child_window(
             title="환자 등록 후 예약", control_type="Button")
         register_btn.wait(wait_for='exists enabled', timeout=30)
         register_btn.click()
 
+    def text_edit_popup(serach_name, phone_number, start_sub_process_event, sub_process_done_event, btn_auto_id):
+        DashBoard.popup_view(serach_name)
+        registration_window = motion_app.window(
+            title=MotionStarter.version_search('고객등록'))
+        edit_window = registration_window.child_window(
+            control_type="Edit", auto_id="txtPat_Nm")
+        edit_window.set_edit_text(serach_name)
+        if phone_number:
+            mobile_edit2 = registration_window.child_window(
+                control_type="Edit", auto_id="txtMobile_No2")
+            mobile_edit3 = registration_window.child_window(
+                control_type="Edit", auto_id="txtMobile_No3")
+            match len(phone_number):
+                case 13:
+                    mobile_edit2.set_edit_text(phone_number[4:8])
+                    mobile_edit3.set_edit_text(phone_number[10:13])
+                case 11:
+                    mobile_edit2.set_edit_text(phone_number[3:7])
+                    mobile_edit3.set_edit_text(phone_number[7:11])
+                case 8:
+                    mobile_edit2.set_edit_text(phone_number[1:4])
+                    mobile_edit3.set_edit_text(phone_number[4:8])
+        save_btn = registration_window.child_window(
+            control_type="Button", auto_id=btn_auto_id)
+        start_sub_process_event.set()
+        save_btn.click()
+        sub_process_done_event.wait()
+
+    def save_receipt_popup(serach_name, phone_number, start_sub_process_event, sub_process_done_event, btn_auto_id):
+        try:
+            DashBoard.text_edit_popup(
+                serach_name, phone_number, start_sub_process_event, sub_process_done_event, btn_auto_id)
+            receipt_window = motion_app.window(
+                title=MotionStarter.version_search('접수'))
+            edit_field = receipt_window.child_window(auto_id="radPanel6")
+            receipt_memo = edit_field.child_window(control_type="Edit")
+            user_memo = edit_field.child_window(control_type="Edit")
+            receipt_memo.set_text("테스트")
+            user_memo.set_text("테스트")
+            receipt_btn = receipt_window.child_window(
+                auto_id="btnAcpt", control_type="Button")
+            start_sub_process_event.set()
+            receipt_btn.click()
+            sub_process_done_event.wait()
+
+            web_window = motion_app.child_winodw(title="Motion E web")
+            acpt_list = web_window.child_window(
+                control_type="List", auto_id="acpt-list")
+            test = acpt_list.children()
+            print(test)
+
+        except Exception as e:
+            if MotionStarter.version_search('고객등록'):
+                registration_window = motion_app.window(
+                    title=MotionStarter.version_search('고객등록'))
+                close_btn = registration_window.child_window(
+                    title="닫기", control_type="Button")
+                close_btn.click()
+                print(e)
+            elif MotionStarter.version_search('접수'):
+                receipt = motion_app.window(
+                    title=MotionStarter.version_search('접수'))
+                close_btn = receipt.child_window(
+                    title="닫기", control_type="Button")
+                close_btn.click()
+                print(e)
+
+    def save_reserve_popup(serach_name, phone_number, start_sub_process_event, sub_process_done_event, btn_auto_id):
+        try:
+            DashBoard.text_edit_popup(
+                serach_name, phone_number, start_sub_process_event, sub_process_done_event, btn_auto_id)
+            time.sleep(2)
+
+        except:
+            if MotionStarter.version_search('고객등록'):
+                receipt_window = motion_app.window(
+                    title=MotionStarter.version_search('고객등록'))
+                close_btn = receipt_window.child_window(
+                    title="닫기", control_type="Button")
+                close_btn.click()
+            else:
+                top_menu = motion_app.child_window(auto_id="pnTop")
+                dashboard_menu = top_menu.child_window(title="Dashboard")
+                dashboard_menu.click_input()
+
+    def user_save(serach_name, phone_number, start_sub_process_event, sub_process_done_event, btn_auto_id):
+        try:
+            DashBoard.text_edit_popup(
+                serach_name, phone_number, start_sub_process_event, sub_process_done_event, btn_auto_id)
+            time.sleep(3)
+
+        except Exception as e:
+            print('저장 실패 : ', e)
+            if MotionStarter.version_search('고객등록'):
+                registration_window = motion_app.window(
+                    title=MotionStarter.version_search('고객등록'))
+                close_btn = registration_window.child_window(
+                    title="닫기", control_type="Button")
+                close_btn.click()
+            keyboard.send_keys('{F5}')
+
     def receipt_card():
         print("")
 
-# 관리자 권한
+    def reserve_card():
+        print("")
 
-    if not is_admin():
-        ctypes.windll.shell32.ShellExecuteW(
-            None, "runas", sys.executable, ' '.join(sys.argv), None, 1)
-        sys.exit()
 
+def main_process_func(start_sub_process_event, sub_process_done_event):
+    # DashBoard.user_save("김지헌", "01074417631",
+    #                     start_sub_process_event, sub_process_done_event, "btnSave")
+    # # sub process unset
+    # sub_process_done_event.clear()
+    # start_sub_process_event.clear()
+    # time(1)
+
+    # DashBoard.save_reserve_popup("김지헌", "01074417631",
+    #                              start_sub_process_event, sub_process_done_event, "btnSaveAcpt")
+    # sub_process_done_event.clear()
+    # start_sub_process_event.clear()
+    # time(1)
+
+    DashBoard.save_receipt_popup("김지헌", "01074417631",
+                                 start_sub_process_event, sub_process_done_event, "btnSave")
+    sub_process_done_event.clear()
+    start_sub_process_event.clear()
+
+
+def sub_process_func(start_sub_process_event, sub_process_done_event, window_auto_id, btn_auto_id):
+    try:
+        start_sub_process_event.wait()
+        registration_window = motion_app.window(
+            title=MotionStarter.version_search('고객등록'))
+        while True:
+            rad_box = registration_window.child_window(
+                auto_id=window_auto_id)
+            # rad_box.wait(wait_for='exists enabled', timeout=30)
+            rad_btn = rad_box.child_window(auto_id=btn_auto_id)
+            rad_btn.click()
+            sub_process_done_event.set()
+            start_sub_process_event.clear()
+    except Exception as e:
+        print("서브 모듈 동작 실패 : ", e)
+
+
+if not is_admin():
+    ctypes.windll.shell32.ShellExecuteW(
+        None, "runas", sys.executable, ' '.join(sys.argv), None, 1)
+    sys.exit()
+win32_app = application.Application(backend='win32')
+motion_app = application.Application(backend='uia')
+MotionStarter.app_connect()
+
+motion_window = motion_app.window(title=MotionStarter.version_search('모션.ver'))
 
 if __name__ == "__main__":
-    win32_app = application.Application(backend='win32')
-    motion_app = application.Application(backend='uia')
 
-    MotionStarter.app_connect()
-    motion_window = motion_app.window(
-        title=MotionStarter.version_search('모션.ver'))
+    start_sub_process_event = multiprocessing.Event()
+    sub_process_done_event = multiprocessing.Event()
 
-    # sub_process_event = multiprocessing.Event()
-    # start_sub_process_event = multiprocessing.Event()
-    # done_sub_process_event = multiprocessing.Event()
-    # main_process = multiprocessing.Process(
-    #     target=main, args=(start_sub_process_event,))
-    # sub_process = multiprocessing.Process(target=process_func, args=())
-    # main_process.start()
-    # sub_process.start()
-    # main_process.join()
+    main_process = multiprocessing.Process(
+        target=main_process_func, args=(start_sub_process_event, sub_process_done_event))
+    sub_process = multiprocessing.Process(
+        target=sub_process_func, args=(start_sub_process_event, sub_process_done_event, "RadMessageBox", "radButton1"))
+
+    main_process.start()
+    sub_process.start()
+
+    main_process.join()
+    sub_process.terminate()
